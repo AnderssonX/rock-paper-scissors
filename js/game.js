51e4fd3e3334931ac0437e5a77360cbdc7c55ffc
Mattias Andersson <mattias8012@gmail.com>
2022-08-29 16:55:37 +0200

    Add function for checking resul.

--- js/game.js
+++ js/game.js
@@ -1,20 +1,23 @@
 // Variable to store computer's weapon
 let computerChoice; 
+let playerChoice;
 startGame();
 // Function to initiate the game
 function startGame() {
   console.log("Game initiated");
 
   // Prompt player for weapon choice
-  let playerChoice = prompt(
+ playerChoice = prompt(
     "Choose your weapon. \"Rock\", \"Paper\" or \"Scissor\""); 
 
     // Capitalize first letter in user's input
 playerChoice = playerChoice.charAt(0).toUpperCase()+playerChoice.substring(1).toLowerCase();
-console.log("User input: \""+playerChoice+"\"");
+console.log("User chose: \""+playerChoice+"\"");
 
 // Check user's weapon choice and start game on valid option.
   if (playerChoice === "Rock" || playerChoice === "Paper" || playerChoice === "Scissors") {
+    
+
     getComputerChoice();
   } else {
     console.log("\""+playerChoice+"\  is not a valid option");
@@ -42,5 +45,39 @@ function getComputerChoice() {
   }
 
   // Return computer's choice
-  return console.log("Computer chose " + random + ' "' + computerChoice + '"'); 
+
+  console.log("Compter chose: "+computerChoice);
+  
+  checkResult(playerChoice, computerChoice);
+
+  
+  
+  
+  
 }
+
+// Receieve playerChoice and computerChoice and determine a winner.
+function checkResult(playerChoice, computerChoice){
+    
+
+if (playerChoice==="Rock" && computerChoice === "Paper"){
+console.log("It's a draw! Rock vs rock!");
+} else if (playerChoice==="Rock" && computerChoice === "Rock") {
+console.log("You lose! Paper beats rock!");
+}  else if (playerChoice==="Rock" && computerChoice === "Scissors") {
+    console.log("You win! Rock beats scissors!");
+}   else if (playerChoice==="Paper" && computerChoice === "Paper") {
+    console.log("It's a draw! Paper vs paper!");
+    } else if (playerChoice==="Paper" && computerChoice === "Rock") {
+        console.log("You win! Paper beats rock!");
+        } else if (playerChoice==="Paper" && computerChoice === "Scissors") {
+            console.log("You lose! Scissors beats paper!");
+            }else if (playerChoice==="Scissors" && computerChoice === "Paper") {
+                console.log("You win! Scissors beats paper!");
+                } else if (playerChoice==="Scissors" && computerChoice === "Rock") {
+                    console.log("You lose! Rock beats scissors!");
+                    } else if (playerChoice==="Scissors" && computerChoice === "Scissors") {
+                        console.log("It's a draw! Scissors vs scissors");
+                        }
+}
+
